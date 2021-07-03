75e34f2f4ab0cbd1f47ae08b129decdd2af1d88b
Crowdbotics <team@crowdbotics.com>
2021-07-03 00:13:12 +0000

    Update models

--- backend/reds_detailing_28399/wsgi.py
+++ backend/reds_detailing_28399/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reds_detailing_28399.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reds_detailing_28399.settings")
 
 application = get_wsgi_application()
